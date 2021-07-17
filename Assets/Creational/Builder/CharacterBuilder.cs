90ff6444a23ac09f883396567230382fb12113be
Kuhpik <improvetheworld@yandex.ru>
2021-07-18 00:49:34 +0300

    Builder

---
+++ Assets/Creational/Builder/CharacterBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using UnityEngine;
+
+namespace Kuhpik.DesignPatterns.Builder
+{
+    public class CharacterBuilder
+    {
+        StringBuilder _builder = new StringBuilder();
+
+        public CharacterBuilder EquipWeapon(string weapon)
+        {
+            _builder.AppendLine($"Weapon: {weapon}");
+            return this;
+        }
+
+        public CharacterBuilder EquipArmor(string armor)
+        {
+            _builder.AppendLine($"Armor: {armor}");
+            return this;
+        }
+
+        public CharacterBuilder EquipPet(string pet)
+        {
+            _builder.AppendLine($"Pet: <color=green>{pet}</color>");
+            return this;
+        }
+
+        public CharacterBuilder EquipBlessing(string blessing)
+        {
+            _builder.AppendLine($"Blessing: <color=orange>{blessing}</color>");
+            return this;
+        }
+
+        public string Get()
+        {
+            _builder.Insert(0, "Character with equipment...\n");
+            string result = _builder.ToString();
+            Debug.Log(result);
+            return result;
+        }
+    }
+}
---
+++ Assets/Creational/Builder/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Kuhpik.DesignPatterns.Builder
+{
+    public class TestScript : MonoBehaviour
+    {
+        [Button("Weapon")]
+        void SimpleCharacter()
+        {
+            var character = new CharacterBuilder().EquipWeapon("Spear").Get();
+        }
+
+        [Button("WithGoodEquipment")]
+        void NiceCharacter()
+        {
+            var character = new CharacterBuilder().EquipWeapon("Excalibur").EquipPet("Octocat")
+                .EquipBlessing("Overspeed").EquipArmor("Undying Set")
+                .Get();
+        }
+    }
+}

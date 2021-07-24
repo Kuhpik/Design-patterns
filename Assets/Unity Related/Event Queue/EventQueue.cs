96505672f38f057bb921479da1ddc7b82677827b
Kuhpik <improvetheworld@yandex.ru>
2021-07-25 01:58:14 +0300

    Event Queue

---
+++ Assets/Unity Related/Event Queue/EventQueue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Kuhpik.DesignPatterns.UnityRelated.EventQueue
+{
+    public class EventQueue
+    {
+        const int _delay = 1;
+        const int _countPerTick = 10;
+
+        readonly List<Action> _eventQueue;
+        bool _isBusy;
+
+        public EventQueue()
+        {
+            _eventQueue = new List<Action>(64);
+        }
+
+        public void Listen(Action action)
+        {
+            _eventQueue.Add(action);
+        }
+
+        public async void Dispatch() 
+        {
+            if (_isBusy) return;
+
+            _isBusy = true;
+            Debug.Log("<color=green>Dispatching new queue</color>");
+
+            var queue = _eventQueue.ToList(); //Copy
+            var countPerTick = 0;
+            _eventQueue.Clear();
+
+            for (int i = 0; i < queue.Count; i++)
+            {
+                queue[i].Invoke();
+
+                countPerTick++;
+                if (countPerTick == _countPerTick)
+                {
+                    countPerTick = 0;
+                    await Task.Delay(_delay);
+                    Debug.Log("<color=orange>Queue tick</color>");
+                }
+            }
+
+            _isBusy = false;
+
+            //Endless loop?
+            if (_eventQueue.Count > 0)
+            {
+                Debug.Log("Queue is not empty. Processing...");
+                Dispatch();
+            }
+        }
+    }
+}
---
+++ Assets/Unity Related/Event Queue/TestScript.cs
@@ -0,0 +1,28 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Kuhpik.DesignPatterns.UnityRelated.EventQueue
+{
+    public class TestScript : MonoBehaviour
+    {
+        [Button]
+        void Test()
+        {
+            var queue = new EventQueue();
+
+            for (int i = 0; i < 25; i++)
+            {
+                queue.Listen(() => Debug.Log("First time"));
+            }
+
+            queue.Dispatch();
+
+            for (int i = 0; i < 7; i++)
+            {
+                queue.Listen(() => Debug.Log("Must not be seen at first dispatch"));
+            }
+
+            queue.Dispatch();
+        }
+    }
+}

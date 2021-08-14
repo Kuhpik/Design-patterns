2100de3db658f864daed589c504aae3e449dc2f0
Kuhpik <improvetheworld@yandex.ru>
2021-08-14 22:16:36 +0300

    Memoizer

---
+++ Assets/Others/Memoizer/Memoizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kuhpik.DesignPatterns.Others.Memoizer
+{
+    public class Memoizer<I, O>
+    {
+        readonly Dictionary<I, O> _cache;
+
+        public Memoizer()
+        {
+            _cache = new Dictionary<I, O>();
+        }
+
+        public O Memoize(I input, Func<I,O> func)
+        {
+            if (_cache.ContainsKey(input)) //This case will much more often
+            {
+                return _cache[input];
+            }
+
+            else
+            {
+                var value = func(input);
+                _cache.Add(input, value);
+                return value;
+            }
+        }
+    }
+}
